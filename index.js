4949e5956f250c5ae5f1091f4bd6cd334b3a086e
Doug Moscrop <doug.moscrop@gmail.com>
2017-08-16 19:51:33 -0400

    Add tests, bump and prune deps

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const loggerExtension = require('./lib/logger-extension');
-
-module.exports = require('./lib/handler');
-
-module.exports.extensions = {
-	logger: loggerExtension
-};
